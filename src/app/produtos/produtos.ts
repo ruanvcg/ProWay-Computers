b0cae0fb81363c81d682e87b2a9bc28dcec0e6fb
Ruan Victor <62728646+RuanCarreiroGomes@users.noreply.github.com>
2023-04-02 20:51:32 -0300

    Atualizando rep remoto.

--- src/app/produtos/produtos.ts
+++ src/app/produtos/produtos.ts
@@ -26,5 +26,5 @@ export const produtos: IProduto[] = [
     { id: 12, descricao: "Mouse barato", preco: 20, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-1.png", quantidadeEstoque: 10 },
     { id: 13, descricao: "Mouse ótimo", preco: 200, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-2.jpg", quantidadeEstoque: 10 },
     { id: 14, descricao: "Mouse pequeno", preco: 50, descricaoPreco: "À vista no PIX", imagem: "./assets/mouse-4.jpg", quantidadeEstoque: 10 },
-    { id: 15, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-2.jpg", quantidadeEstoque: 10 },
+    { id: 15, descricao: "Teclado bom", preco: 159.99, descricaoPreco: "À vista no PIX", imagem: "./assets/teclado-2.jpg", quantidadeEstoque: 8 },
 ]
